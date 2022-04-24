5b873df9439877e93d88828ccf54d08c3a32f16f
SergeyKKK <skusnarev3@gmail.com>
2022-04-25 00:10:03 +0300

    overrideUserInterfaceStyle = .light

--- Opinion/Opinion/AppDelegate/AppDelegate.swift
+++ Opinion/Opinion/AppDelegate/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = welcomeViewController
+        window?.overrideUserInterfaceStyle = .light
         window?.makeKeyAndVisible()
         
         return true
