a55784abdb43384a0cb1aa8b1af410eca89809b4
SergeyKKK <skusnarev3@gmail.com>
2022-05-03 13:36:26 +0300

    #4 - create EnteranceViewController + refactoring

---
+++ Opinion/Opinion/Controllers/EntranceViewController/EntranceView.swift
@@ -0,0 +1,8 @@
+//
+//  EntranceView.swift
+//  Opinion
+//
+//  Created by Serega Kushnarev on 25.04.2022.
+//
+
+import Foundation
---
+++ Opinion/Opinion/Controllers/EntranceViewController/EntranceViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EntranceViewController.swift
+//  Opinion
+//
+//  Created by Serega Kushnarev on 25.04.2022.
+//
+
+import UIKit
+
+class EntranceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
